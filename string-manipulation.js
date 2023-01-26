eeec6cab3a569e3210a4880f18cee4c9005e3873
Subham Sahu <83798604+drcount-root@users.noreply.github.com>
2023-01-26 06:12:46 +0000

    ok

--- string-manipulation.js
+++ string-manipulation.js
@@ -5,8 +5,8 @@ const stringManipFunc = str => {
     const arrStr = str.toLowerCase().split(' ');
     // let result = '';
     for (let i in arrStr) {
-      // result += arrStr[i][0].toUpperCase() + arrStr[i].substr(1) + ' ';
-      arrStr[i] = arrStr[i][0].toUpperCase() + arrStr[i].substr(1);
+      // result += arrStr[i][0].toUpperCase() + arrStr[i].substring(1) + ' ';
+      arrStr[i] = arrStr[i][0].toUpperCase() + arrStr[i].substring(1);
     }
     // return result; // length => 13
     return arrStr.join(' '); // length => 12
