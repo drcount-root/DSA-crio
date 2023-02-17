8e5aad4af5e4269c28578700c5dab81762e1e20b
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-17 20:58:25 +0530

    .

--- Crio-DSA-2/W2/find-largest-subarray-with-sum-zero.js
+++ Crio-DSA-2/W2/find-largest-subarray-with-sum-zero.js
@@ -1,72 +1,43 @@
-/*
+function largestSubarrayWithSumZero(n, arr) {
+  const sumMap = new Map(); // create an empty map to store prefix sums and their indices
+  let sum = 0; // initialize the prefix sum to 0
+  let maxLength = 0; // initialize the maximum length of a subarray with sum 0 to 0
+  let maxStartIndex = -1; // initialize the starting index of the subarray to -1
 
-Given an integer array, find the largest subarray with sum 0 . If there is more than one subarray with the largest length, return the subarray with the lowest starting index.
-
-If there is no such sub-array print -1.
-
-Input format
-There are 2 lines of input.
-
-First line contains an integer N.
-
-Next line consists of N integers, the values of the array.
-
-Output format
-Print the elements of the sub-array with spaces which have the largest length and sum of elements equal to zero.
-
-Sample Input 1
-6
-
-2 3 1 -4 0 6
-
-Sample Output 1
-3 1 -4 0
-
-Explanation
-The largest subarray with sum zero is (3, 1, -4, 0)
-
-Constraints
-1 <= N <= 10^5
-
--10^4 <= arr[i] <= 10^4
-
-*/
-
-function largestSubarraySumZero(n, arr) {
-  let sumMap = new Map();
-  let maxLen = 0;
-  let maxStartIndex = -1;
-  let sum = 0;
-
-  sumMap.set(0, -1); // add a default entry to handle edge case
+  sumMap.set(0, -1); // set the initial sum of 0 at index -1
 
   for (let i = 0; i < n; i++) {
-    sum += arr[i];
+    sum += arr[i]; // add the current element to the prefix sum
 
     if (sumMap.has(sum)) {
-      let startIndex = sumMap.get(sum) + 1;
-      let len = i - startIndex + 1;
+      // check if the current sum has been seen before
+      const startIndex = sumMap.get(sum) + 1; // get the starting index of the subarray with sum 0
 
-      if (len > maxLen) {
-        maxLen = len;
+      if (i - startIndex > maxLength) {
+        // check if the current subarray is longer than the previous one
+        maxLength = i - startIndex;
         maxStartIndex = startIndex;
       }
     } else {
-      sumMap.set(sum, i);
+      sumMap.set(sum, i); // store the current sum and its index in the map
     }
   }
 
-  if (maxLen > 0) {
-    return arr.slice(maxStartIndex, maxStartIndex + maxLen).join(" ");
-  } else {
+  if (maxStartIndex === -1) {
+    // check if no subarray with sum 0 was found
+
     return -1;
   }
+
+  const answer = [];
+
+  for (let i = maxStartIndex; i <= maxStartIndex + maxLength; i++) {
+    // print the elements of the subarray
+    // console.log(`${arr[i]} `);
+    answer.push(arr[i]);
+  }
+  //   console.log();
+  return answer;
 }
 
-console.log(largestSubarraySumZero(6, [2, 3, 1, -4, 0, 6]));
-
-console.log(largestSubarraySumZero(5, [1, 2, 3, 4, 5]));
-
-console.log(largestSubarraySumZero(7, [-1, 1, 2, -2, -1, 3, -3]));
-
-console.log(largestSubarraySumZero(5, [-2, 2, 0, 3, -3]));
+console.log(largestSubarrayWithSumZero(6, [2, 3, 1, -4, 0, 6]));
