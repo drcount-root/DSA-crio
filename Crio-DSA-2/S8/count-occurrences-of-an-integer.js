d1201419e3aaa7af0cebf79b42560af22dd65f67
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-27 17:51:11 +0530

    q

---
+++ Crio-DSA-2/S8/count-occurrences-of-an-integer.js
@@ -0,0 +1,33 @@
+/*
+
+Given a sorted integer array of length n with possible duplicate elements. Find the number of occurrences of an integer k using binary search.
+
+Input format
+There are 2 lines of input.
+
+First line contains 2 space separated integers n, k - Number of elements, the integer k.
+
+Second line contains n space separated integers - The integer array.
+
+Output format
+Print the number of occurrences of the integer k.
+
+Sample Input 1
+5 2
+
+-1 2 2 4 7
+
+Sample Output 1
+2
+
+Explanation 1
+The integer 2 occurs 2 times in the given array.
+
+Constraints
+1 <= n <= 10^6
+
+-10^9 <= k <= 10^9
+
+-10^9 <= A[i] <= 10^9
+
+*/
