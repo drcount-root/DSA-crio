84d3baab9ed9460faab5365e27af327429cd746b
Subham Sahu <about.subhamsahu@gmail.com>
2023-03-01 22:21:01 +0530

    .

---
+++ L-Squared/matrix-addition.js
@@ -0,0 +1,33 @@
+function additionOfMatrix(matrix1, matrix2) {
+  let n = matrix1.length;
+  let m = matrix1[0].length;
+
+  let ans = new Array(n);
+  for (let k = 0; k < n; k++) {
+    ans[k] = new Array(m);
+  }
+
+  for (let i = 0; i < n; i++) {
+    //row
+    for (let j = 0; j < m; j++) {
+      //col
+      ans[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+  }
+  return ans;
+}
+
+console.log(
+  additionOfMatrix(
+    [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ],
+    [
+      [10, 11, 12],
+      [13, 14, 15],
+      [16, 17, 18],
+    ]
+  )
+);
